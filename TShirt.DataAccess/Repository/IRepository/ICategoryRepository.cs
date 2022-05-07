ecadac76ad212162fa1a1f9b28f84a28ebaa8cae
TibZz <thibaut_marechal@yahoo.com>
2022-05-07 15:18:36 +0100

    interface for category repository

---
+++ TShirt.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TShirt.Models;
+
+namespace TShirt.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        // When doing IRepo the Model will be Category, so will get all methods implemented inside the generic repository
+        // Here we need to implement method to udpate the Category as it will be specific to each Model
+
+        void Update(Category obj); 
+
+    }
+}
