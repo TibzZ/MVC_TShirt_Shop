6daeb03fad688e6542485c5b3799c6aeea70cfe3
TibZz <thibaut_marechal@yahoo.com>
2022-05-07 14:55:00 +0100

    Start Repository Pattern - generic IRepository

---
+++ TShirt.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TShirt.DataAccess.Repository.IRepository
+{
+    //generic repository where T is a class like Category or Order
+    //Update methods are specific to each class so not to include in generic repo
+    public interface IRepository<T> where T : class
+    {
+        T GetFirstOrDefault(Expression<Func<T,bool>> filter); 
+        IEnumerable<T> GetAll();    
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+
+    }
+}
